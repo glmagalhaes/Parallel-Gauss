3749eab19f159020691d97c7eb79bf2e6b9e5cde
Rafael <rafael@rafael-Aspire-E1-571.(none)>
2015-05-17 18:30:22 -0300

    alguma coisa

--- src/Clik-PLus/Cilk.cpp
+++ src/Clik-PLus/Cilk.cpp
@@ -6,54 +6,44 @@
 
 void swap_row(double *a, double *b, int r1, int r2, int n)
 {
-	double tmp2;
+	double tmp, *p1, *p2;
 	int i;
 
 	if (r1 == r2) return;
-	#pragma omp parallel for
-	for (i = 0; i < n; i++) {
-        double tmp, *p1, *p2;
+	cilk_for (i = 0; i < n; i++) {
 		p1 = mat_elem(a, r1, i, n);
 		p2 = mat_elem(a, r2, i, n);
 		tmp = *p1, *p1 = *p2, *p2 = tmp;
 	}
+	tmp = b[r1], b[r1] = b[r2], b[r2] = tmp;
 }
 
 void gauss_eliminate(double *a, double *b, double *x, int n)
 {
 #define A(y, x) (*mat_elem(a, y, x, n))
 	int i, j, col, row, max_row,dia;
-	double max;
+	double max, tmp;
 
-	for (dia = 0; dia < n; dia++) {
+	cilk_for (dia = 0; dia < n; dia++) {
 		max_row = dia, max = A(dia, dia);
 
-        #pragma omp parallel for
-		for (row = dia + 1; row < n; row++){
-            double tmp;
+		cilk_for (row = dia + 1; row < n; row++)
 			if ((tmp = fabs(A(row, dia))) > max)
 				max_row = row, max = tmp;
-		}
+
 		swap_row(a, b, dia, max_row, n);
 
-        #pragma omp parallel for
-		for (row = dia + 1; row < n; row++) {
-            double tmp;
+		cilk_for (row = dia + 1; row < n; row++) {
 			tmp = A(row, dia) / A(dia, dia);
-
-			for (col = dia+1; col < n; col++)
+			cilk_for (col = dia+1; col < n; col++)
 				A(row, col) -= tmp * A(dia, col);
 			A(row, dia) = 0;
 			b[row] -= tmp * b[dia];
 		}
 	}
-
-    #pragma omp parallel for
-	for (row = n - 1; row >= 0; row--) {
-        double tmp;
+	cilk_for (row = n - 1; row >= 0; row--) {
 		tmp = b[row];
-
-		for (j = n - 1; j > row; j--)
+		cilk_for (j = n - 1; j > row; j--)
 			tmp -= x[j] * A(row, j);
 		x[row] = tmp / A(row, row);
 	}
