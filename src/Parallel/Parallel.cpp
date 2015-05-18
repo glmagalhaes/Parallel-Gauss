2a74b765c3e8408133930a6e0f4dcae83dc6e345
Rafael <rafael@rafael-Aspire-E1-571.(none)>
2015-05-18 01:36:58 -0300

    Parallel

--- src/Parallel/Parallel.cpp
+++ src/Parallel/Parallel.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <ppl.h>
 
 #define mat_elem(a, y, x, n) (a + ((y) * (n) + (x)))
 
@@ -10,13 +11,14 @@ void swap_row(double *a, double *b, int r1, int r2, int n)
 	int i;
 
 	if (r1 == r2) return;
-	#pragma omp parallel for
-	for (i = 0; i < n; i++) {
+	parallel_for(i(0),n,[&](i i)
+	 {
         double tmp, *p1, *p2;
 		p1 = mat_elem(a, r1, i, n);
 		p2 = mat_elem(a, r2, i, n);
 		tmp = *p1, *p1 = *p2, *p2 = tmp;
-	}
+	});
+	
 	tmp2 = b[r1], b[r1] = b[r2], b[r2] = tmp2;
 }
 
@@ -25,11 +27,11 @@ void gauss_eliminate(double *a, double *b, double *x, int n)
 #define A(y, x) (*mat_elem(a, y, x, n))
 	int i, j, col, row, max_row,dia;
 	double max;
-	
+
 	for (dia = 0; dia < n; dia++) {
 		max_row = dia, max = A(dia, dia);
-		
-        #pragma omp parallel for
+
+        //#pragma omp parallel for
 		for (row = dia + 1; row < n; row++){
             double tmp;
 			if ((tmp = fabs(A(row, dia))) > max)
@@ -41,18 +43,18 @@ void gauss_eliminate(double *a, double *b, double *x, int n)
 		for (row = dia + 1; row < n; row++) {
             double tmp;
 			tmp = A(row, dia) / A(dia, dia);
-			#pragma omp parallel for
+			//#pragma omp parallel for
 			for (col = dia+1; col < n; col++)
 				A(row, col) -= tmp * A(dia, col);
 			A(row, dia) = 0;
 			b[row] -= tmp * b[dia];
 		}
 	}
-	#pragma omp parallel for
+	//#pragma omp parallel for
 	for (row = n - 1; row >= 0; row--) {
         double tmp;
 		tmp = b[row];
-		#pragma omp parallel for
+		//#pragma omp parallel for
 		for (j = n - 1; j > row; j--)
 			tmp -= x[j] * A(row, j);
 		x[row] = tmp / A(row, row);
